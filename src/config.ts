0eba5df3ae9ab8b480764ab24338583c8f4c24de
hudshaugdaa <84747212+hudshaugdaa@users.noreply.github.com>
2026-04-20 22:52:28 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const FUNNEL_CONFIG = {
   originalPrice: "R$ 97,00",
   salePrice: "R$ 27,90",
-  paymentLink: "https://pay.hotmart.com/YOURLINK",
+  paymentLink: "https://app.pushinpay.com.br/service/pay/A197DD5C-3237-416D-A87F-CE73C1B6810D",
   companyName: "Proteja Seu Relacionamento",
   year: 2024,
 };
